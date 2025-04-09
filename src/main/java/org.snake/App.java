7d985d6c1741db3070be57a0c02384c2b41bb471
Sus4n00 <96766854+Sus4no@users.noreply.github.com>
2025-04-09 12:11:55 +0300

    Create App.java

---
+++ src/main/java/org.snake/App.java
@@ -0,0 +1,19 @@
+package org.snake;
+
+import java.awt.EventQueue;
+
+public class App {
+    public static GUI gui;
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    gui = new GUI();
+                    gui.createGameWindow();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
